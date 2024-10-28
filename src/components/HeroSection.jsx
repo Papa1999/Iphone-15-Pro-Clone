f4b7c4dc1d832040b793837a7924ca380d123f22
Papa1999 <thioubepapamacoumba@gmail.com>
2024-10-28 17:40:23 +0100

    Hero Video using size

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,14 +1,37 @@
 import { useGSAP } from "@gsap/react";
-import { hero } from "../assets/videos";
+import { hero, smallHero } from "../assets/videos";
 import gsap from "gsap";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function HeroSection() {
   useGSAP(() => {
-    gsap.to("#heroTitle", {opacity:1, y:0, delay:1.1});
+    gsap.to("#heroTitle", { opacity: 1, y: 0, delay: 1.1 });
     gsap.to("#video_wrapper", { opacity: 1, y: 0 });
     gsap.to("#video_wrapper", { opacity: 1 });
-    gsap.to("#btn", { opacity: 1, y:0,  delay: 1.1 });
+    gsap.to("#btn", { opacity: 1, y: 0, delay: 1.1 });
+    gsap.to("#paragraph", { opacity: 1, y: 0, delay: 1.1 });
   }, []);
+
+  const [heroVideo, setheroVideo] = useState(
+    window.innerWidth < 768 ? smallHero : hero
+  );
+
+  const handleSize = () => {
+    if (window.innerWidth < 768) {
+      setheroVideo(smallHero);
+    } else {
+      setheroVideo(hero);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleSize);
+
+    return () => {
+      window.removeEventListener("resize", handleSize);
+    };
+  });
 
   return (
     <section className="container flex flex-col items-center text-white h-screen justify-evenly">
@@ -17,13 +40,20 @@ export default function HeroSection() {
       </h2>
       <div id="video_wrapper" className="opacity-0">
         <video autoPlay loop muted>
-          <source src={hero} />
+          <source src={heroVideo} />
         </video>
       </div>
 
-      <button className="btn opacity-0 translate-y-[-20px]" id="btn">Buy</button>
+      <button className="btn opacity-0 translate-y-[50px]" id="btn">
+        Buy
+      </button>
       <div>
-        <p className="text-[20px] font-poppins">From $199/month or $999. </p>
+        <p
+          id="paragraph"
+          className="text-[20px] font-poppins translate-y-[50px] opacity-0"
+        >
+          From $199/month or $999.
+        </p>
       </div>
     </section>
   );
